43300d4bab9759b972259e9c833ed1128c54f351
Jonathan van der Meulen <jonajvdm@gmail.com>
2024-12-17 15:10:00 +0100

    clean up test for day 17

--- day17/day17_test.go
+++ day17/day17_test.go
@@ -20,260 +20,135 @@ func BenchmarkRun(b *testing.B) {
 
 func TestMachine_Simulate(t *testing.T) {
 	testCases := []struct {
-		name   string
-		in     day17.Machine
-		out    day17.Machine
-		outStr string
+		name         string
+		a1, b1, c1   int
+		a2, b2, c2   int
+		instructions []int
+		out          string
 	}{
 		{
-			name: "example case",
-			in: day17.Machine{
-				A:    729,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 1, 5, 4, 3, 0},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 1, 5, 4, 3, 0},
-			},
-			outStr: "4,6,3,5,6,3,5,2,1,0",
+			"example case",
+			729, 0, 0,
+			0, 0, 0,
+			[]int{0, 1, 5, 4, 3, 0},
+			"4,6,3,5,6,3,5,2,1,0",
 		},
 		{
-			name: "Small example 1",
-			in: day17.Machine{
-				A:    0,
-				B:    0,
-				C:    9,
-				Inst: []int{2, 6},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    1,
-				C:    9,
-				Inst: []int{2, 6},
-			},
-			outStr: "",
+			"Small example 1",
+			0, 0, 9,
+			0, 1, 9,
+			[]int{2, 6},
+			"",
 		},
 		{
-			name: "Small example 2",
-			in: day17.Machine{
-				A:    10,
-				B:    0,
-				C:    0,
-				Inst: []int{5, 0, 5, 1, 5, 4},
-			},
-			out: day17.Machine{
-				A:    10,
-				B:    0,
-				C:    0,
-				Inst: []int{5, 0, 5, 1, 5, 4},
-			},
-			outStr: "0,1,2",
+			"Small example 2",
+			10, 0, 0,
+			10, 0, 0,
+			[]int{5, 0, 5, 1, 5, 4},
+			"0,1,2",
 		},
 		{
-			name: "Small example 3",
-			in: day17.Machine{
-				A:    2024,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 1, 5, 4, 3, 0},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 1, 5, 4, 3, 0},
-			},
-			outStr: "4,2,5,6,7,7,7,7,3,1,0",
+			"Small example 3",
+			2024, 0, 0,
+			0, 0, 0,
+			[]int{0, 1, 5, 4, 3, 0},
+			"4,2,5,6,7,7,7,7,3,1,0",
 		},
 		{
-			name: "Small example 4",
-			in: day17.Machine{
-				A:    0,
-				B:    29,
-				C:    0,
-				Inst: []int{1, 7},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    26,
-				C:    0,
-				Inst: []int{1, 7},
-			},
-			outStr: "",
+			"Small example 4",
+			0, 29, 0,
+			0, 26, 0,
+			[]int{1, 7},
+			"",
 		},
 		{
-			name: "Small example 5",
-			in: day17.Machine{
-				A:    0,
-				B:    2024,
-				C:    43690,
-				Inst: []int{4, 0},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    44354,
-				C:    43690,
-				Inst: []int{4, 0},
-			},
-			outStr: "",
+			"Small example 5",
+			0, 2024, 43690,
+			0, 44354, 43690,
+			[]int{4, 0},
+			"",
 		},
 		{
-			name: "Opcode 0",
-			in: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 2},
-			},
-			out: day17.Machine{
-				A:    3,
-				B:    0,
-				C:    0,
-				Inst: []int{0, 2},
-			},
-			outStr: "",
+			"Opcode 0",
+			12, 0, 0,
+			3, 0, 0,
+			[]int{0, 2},
+			"",
 		},
 		{
-			name: "Opcode 1",
-			in: day17.Machine{
-				A:    0,
-				B:    12,
-				C:    0,
-				Inst: []int{1, 5},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    9,
-				C:    0,
-				Inst: []int{1, 5},
-			},
-			outStr: "",
+			"Opcode 1",
+			0, 12, 0,
+			0, 9, 0,
+			[]int{1, 5},
+			"",
 		},
 		{
-			name: "Opcode 2",
-			in: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{2, 4},
-			},
-			out: day17.Machine{
-				A:    12,
-				B:    4,
-				C:    0,
-				Inst: []int{2, 4},
-			},
-			outStr: "",
+			"Opcode 2",
+			12, 0, 0,
+			12, 4, 0,
+			[]int{2, 4},
+			"",
 		},
 		{
 			// Note that this instruction assumes that opcode 5 works
-			name: "Opcode 3 (zero value)",
-			in: day17.Machine{
-				A:    0,
-				B:    0,
-				C:    0,
-				Inst: []int{3, 5, 5, 1},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    0,
-				C:    0,
-				Inst: []int{3, 5, 5, 1},
-			},
-			outStr: "1",
+			"Opcode 3 (zero value)",
+			0, 0, 0,
+			0, 0, 0,
+			[]int{3, 5, 5, 1},
+			"1",
 		},
 		{
 			// Note that this instruction assumes that opcode 5 works
-			name: "Opcode 3 (non-zero value)",
-			in: day17.Machine{
-				A:    1,
-				B:    0,
-				C:    0,
-				Inst: []int{3, 5, 5, 1},
-			},
-			out: day17.Machine{
-				A:    1,
-				B:    0,
-				C:    0,
-				Inst: []int{3, 5, 5, 1},
-			},
-			outStr: "",
+			"Opcode 3 (non-zero value)",
+			1, 0, 0,
+			1, 0, 0,
+			[]int{3, 5, 5, 1},
+			"",
 		},
 		{
-			name: "Opcode 4",
-			in: day17.Machine{
-				A:    0,
-				B:    12,
-				C:    5,
-				Inst: []int{4, 123},
-			},
-			out: day17.Machine{
-				A:    0,
-				B:    9,
-				C:    5,
-				Inst: []int{4, 123},
-			},
-			outStr: "",
+			"Opcode 4",
+			0, 12, 5,
+			0, 9, 5,
+			[]int{4, 123},
+			"",
 		},
 		{
-			name: "Opcode 5",
-			in: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{5, 1, 5, 4},
-			},
-			out: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{5, 1, 5, 4},
-			},
-			outStr: "1,4",
+			"Opcode 5",
+			12, 0, 0,
+			12, 0, 0,
+			[]int{5, 1, 5, 4},
+			"1,4",
 		},
 		{
-			name: "Opcode 6",
-			in: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{6, 2},
-			},
-			out: day17.Machine{
-				A:    12,
-				B:    3,
-				C:    0,
-				Inst: []int{6, 2},
-			},
-			outStr: "",
+			"Opcode 6",
+			12, 0, 0,
+			12, 3, 0,
+			[]int{6, 2},
+			"",
 		},
 		{
-			name: "Opcode 7",
-			in: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    0,
-				Inst: []int{7, 2},
-			},
-			out: day17.Machine{
-				A:    12,
-				B:    0,
-				C:    3,
-				Inst: []int{7, 2},
-			},
-			outStr: "",
+			"Opcode 6",
+			12, 0, 0,
+			12, 0, 3,
+			[]int{7, 2},
+			"",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			out := tc.in.Simulate()
-			assert.Equal(t, tc.out, tc.in)
-			assert.Equal(t, tc.outStr, out)
+			in := day17.Machine{
+				tc.a1, tc.b1, tc.c1,
+				tc.instructions,
+			}
+			out := day17.Machine{
+				tc.a2, tc.b2, tc.c2,
+				tc.instructions,
+			}
+
+			output := in.Simulate()
+			assert.Equal(t, out, in)
+			assert.Equal(t, tc.out, output)
 		})
 	}
 }
