394b02d438a31f3a522a3fa05413ab11cf41c5ef
Jonathan van der Meulen <jonajvdm@gmail.com>
2024-12-18 16:29:07 +0100

    add bfs util function

--- day18/day18.go
+++ day18/day18.go
@@ -11,81 +11,6 @@ import (
 
 const SIZE int = 70
 
-type Room struct {
-	Room   map[int]map[int]bool
-	Range  int
-	Coords []utils.Point
-	Prev   []utils.Point
-}
-
-func (r *Room) Set(x, y int) {
-	if _, ok := r.Room[x]; !ok {
-		r.Room[x] = make(map[int]bool)
-	}
-	r.Room[x][y] = true
-}
-
-func (r *Room) BFS() int {
-	start := utils.Point{X: 0, Y: 0}
-	goal := utils.Point{X: r.Range, Y: r.Range}
-
-	q := make([]utils.Point, 1)
-	q[0] = start
-	prev := make(map[utils.Point]utils.Point)
-
-	r.Prev = make([]utils.Point, 0)
-
-	for len(q) > 0 {
-		i := q[0]
-		q = q[1:]
-
-		if i == goal {
-			var count int
-			n := i
-			for {
-				n = prev[n]
-				r.Prev = append(r.Prev, n)
-				count++
-				if n == start {
-					break
-				}
-			}
-			return count
-		}
-
-		for _, n := range utils.GetDirectAdjacend(i.X, i.Y, r.Range+1, r.Range+1) {
-			if _, ok := prev[n]; ok || r.Room[n.X][n.Y] {
-				continue
-			}
-
-			prev[n] = i
-			q = append(q, n)
-		}
-	}
-
-	return -1
-}
-
-func (r *Room) FindLast(minNum int) string {
-	r.BFS()
-	for {
-		minNum++
-		r.Set(r.Coords[minNum].X, r.Coords[minNum].Y)
-		if !slices.Contains(r.Prev, r.Coords[minNum]) {
-			continue
-		}
-		if r.BFS() == -1 {
-			break
-		}
-	}
-
-	return fmt.Sprintf(
-		"%d,%d",
-		r.Coords[minNum].X,
-		r.Coords[minNum].Y,
-	)
-}
-
 func Run(file string) [2]interface{} {
 	data := utils.ReadFile(file)
 
@@ -122,4 +47,60 @@ func Parse(data []string, amount int) Room {
 	}
 
 	return room
+}
+
+type Room struct {
+	Room   map[int]map[int]bool
+	Range  int
+	Coords []utils.Point
+	Prev   []utils.Point
+}
+
+func (r *Room) Set(x, y int) {
+	if _, ok := r.Room[x]; !ok {
+		r.Room[x] = make(map[int]bool)
+	}
+	r.Room[x][y] = true
+}
+
+func (r *Room) Next(p utils.Point) []utils.Point {
+	next := make([]utils.Point, 0)
+
+	for _, n := range utils.GetDirectAdjacend(p.X, p.Y, r.Range+1, r.Range+1) {
+		if !r.Room[n.X][n.Y] {
+			next = append(next, n)
+		}
+	}
+
+	return next
+}
+
+func (r *Room) BFS() int {
+	start := utils.Point{X: 0, Y: 0}
+	goal := utils.Point{X: r.Range, Y: r.Range}
+
+	path := utils.BFS(start, goal, r)
+	r.Prev = path
+
+	return len(path) - 1
+}
+
+func (r *Room) FindLast(minNum int) string {
+	r.BFS()
+	for {
+		minNum++
+		r.Set(r.Coords[minNum].X, r.Coords[minNum].Y)
+		if !slices.Contains(r.Prev, r.Coords[minNum]) {
+			continue
+		}
+		if r.BFS() == -1 {
+			break
+		}
+	}
+
+	return fmt.Sprintf(
+		"%d,%d",
+		r.Coords[minNum].X,
+		r.Coords[minNum].Y,
+	)
 }
